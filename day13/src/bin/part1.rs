6f24f40a3cbee7511bb01b7e110e21120d31587d
yabutan <yabutanpro@gmail.com>
2022-12-14 21:22:35 +0900

    day13 setup

---
+++ day13/src/bin/part1.rs
@@ -0,0 +1,9 @@
+use std::fs::File;
+use std::io::BufReader;
+
+fn main() -> anyhow::Result<()> {
+    let r = BufReader::new(File::open("./day13/data/input.txt")?);
+
+    // println!("answer: {}", path.len() - 1);
+    Ok(())
+}
