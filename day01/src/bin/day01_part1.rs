a2eccba503c4effc35fbf09c59f19253d68ec9e3
yabutan <yabutanpro@gmail.com>
2023-01-01 16:56:53 +0900

    day01 refactoring

--- day01/src/bin/day01_part1.rs
+++ day01/src/bin/day01_part1.rs
@@ -1,82 +1,14 @@
 use std::fs;
-use std::io::{BufRead, BufReader};
+use std::io::BufReader;
 
 use anyhow::Result;
-
-fn get_max_sum(r: impl BufRead) -> Result<u32> {
-    let mut max = 0;
-    let mut sum = 0;
-    for line in r.lines() {
-        let line = line?;
-
-        println!("line: {}", line);
-
-        if line.is_empty() {
-            if sum > max {
-                max = sum;
-            }
-            sum = 0;
-            continue;
-        }
-
-        let value: u32 = line.trim().parse()?;
-        sum += value;
-    }
-
-    if sum > max {
-        max = sum;
-    }
-
-    Ok(max)
-}
+use day01::read_sum_list;
 
 fn main() -> Result<()> {
-    let r = BufReader::new(fs::File::open("./day01/data/input.txt").unwrap());
-    let max = get_max_sum(r)?;
+    let r = BufReader::new(fs::File::open("./day01/data/input.txt")?);
+    let list = read_sum_list(r)?;
+    let max = *list.iter().max().expect("No max found");
+
     println!("answer: {}", max);
     Ok(())
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_get_sum_max() {
-        let lines = r#"1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000
-"#;
-
-        let max = get_max_sum(lines.as_bytes()).expect("failed to get max sum");
-        assert_eq!(max, 24000);
-
-        let lines = r#"1000
-
-10000
-10000
-"#;
-
-        let max = get_max_sum(lines.as_bytes()).expect("failed to get max sum");
-        assert_eq!(max, 20000);
-
-        let lines = r#"1000
-
-10000
-10000"#;
-
-        let max = get_max_sum(lines.as_bytes()).expect("failed to get max sum");
-        assert_eq!(max, 20000);
-    }
 }
--- day01/src/bin/day01_part2.rs
+++ day01/src/bin/day01_part2.rs
@@ -1,73 +1,16 @@
 use std::fs;
-use std::io::{BufRead, BufReader};
+use std::io::BufReader;
 
 use anyhow::Result;
-
-fn get_sum_list(r: impl BufRead) -> Result<Vec<u32>> {
-    let mut list = vec![];
-    let mut sum = 0;
-    for line in r.lines() {
-        let line = line?;
-
-        if line.is_empty() {
-            list.push(sum);
-            sum = 0;
-            continue;
-        }
-
-        let value: u32 = line.trim().parse()?;
-        sum += value;
-    }
-
-    if sum > 0 {
-        list.push(sum);
-    }
-
-    Ok(list)
-}
-
-fn get_sum_of_top_tree(r: impl BufRead) -> Result<u32> {
-    let mut list = get_sum_list(r)?;
-    list.sort();
-    list.reverse();
-
-    println!("list: {:?}", list);
-
-    let sum: u32 = list.iter().take(3).sum();
-
-    Ok(sum)
-}
+use day01::read_sum_list;
+use itertools::Itertools;
 
 fn main() -> Result<()> {
-    let r = BufReader::new(fs::File::open("./day01/data/input.txt").unwrap());
-    let sum = get_sum_of_top_tree(r)?;
-    println!("answer: {}", sum);
+    let r = BufReader::new(fs::File::open("./day01/data/input.txt")?);
+    let list = read_sum_list(r)?;
+
+    let sum_of_top_three: u32 = list.iter().sorted().rev().take(3).sum();
+    println!("answer: {}", sum_of_top_three);
+
     Ok(())
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_get_sum_of_top_tree() {
-        let lines = r#"1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000
-"#;
-
-        let sum = get_sum_of_top_tree(lines.as_bytes()).expect("failed to get max sum");
-        assert_eq!(sum, 45000);
-    }
 }
